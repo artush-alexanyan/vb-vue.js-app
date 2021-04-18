3a6642b8e741a529b0bcca50ba2a1e0642b32139
artush-alexanyan <artush.alexanyan1989@gmail.com>
2021-04-18 09:33:18 -0700

    initial commit

---
+++ src/components/Home.vue
@@ -0,0 +1,338 @@
+<template>
+  <div class="home">
+        <section id="navbar">
+        <nav class="navbar navbar-expand-lg navbar-light">
+            <a class="navbar-brand" href="#">
+                <img src="../assets/images/logo.svg" alt="" class="img-fluid">
+            </a>
+            <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId"
+                aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="collapsibleNavId">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Docs</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Components</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Directives</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Icons</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Referance</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Themes</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Play</a>
+                    </li>
+                </ul>
+                <ul class="navbar-nav">
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">v2.21.2</a>
+                        <div class="dropdown-menu" aria-labelledby="dropdownId">
+                            <a class="dropdown-item" href="#">Latest (v2.21.2)</a>
+                            <a class="dropdown-item" href="#">Development</a>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">
+                            <img src="../assets/images/github.svg" alt="" class="img-fluid">
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="" class="nav-link">
+                            <img src="../assets/images/tweeter.svg" alt="" class="img-fluid">
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="" class="nav-link">
+                            <img src="../assets/images/mess.svg" alt="" class="img-fluid">
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="" class="nav-link">
+                            <img src="../assets/images/circle.svg" alt="" class="img-fluid">
+                        </a>
+                    </li>                    
+                </ul>
+            </div>
+        </nav>
+    </section>
+
+    <section id="hero">
+        <div class="container">
+            <div class="row d-flexjustify-content-center">
+                <div class="col-md-8 my-5">
+                    <h1 class="mt-5">Bootstrap<span class="vue">Vue</span></h1>
+                    <p class="mt-3 hero-text">
+                            With BootstrapVue you can build responsive, mobile-first, and ARIA accessible projects on the web using 
+                         <span class="hero-text-span">Vue.js</span> 
+                            and the world's most popular front-end CSS library —
+                         <span class="hero-text-span">Bootstrap v4</span>
+                    </p>
+                    <div class="d-flex">
+                        <img src="../assets/images/logo-2.svg" alt="" class="img-fluid">
+                        <a href="Bootstrap v4" class="ml-4 mt-2">Bootstrapv4</a>
+                        <p class="ml-3 mt-2 text-muted">is the world's most popular framework for building responsive, mobile-first sites.</p>
+                    </div>
+                    <div class="d-flex mb-4">
+                        <img src="../assets/images/vue-logo.svg" alt="" class="img-fluid">
+                        <a href="Bootstrap v4" class="ml-4 mt-2">Vue.js</a>
+                        <p class="ml-3 mt-2 text-muted">is the world's most popular framework for building responsive, mobile-first sites.</p>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <img src="../assets/images/big-logo.svg" alt="" class="img-fluid vue-vuutstrap-img mt-5">
+                </div>
+                <div class="col-md-12 mt-5">
+                    <p class="mb-0 text-muted text-center mb-n4 mb-md-n5">Current Version
+                        <br>
+                        <a href="/docs/reference/changelog"  target="_self" class="small " data-v-792738cd="">v2.21.2</a>
+                    </p> 
+                </div>
+            </div>
+        </div>
+    </section>
+    
+    <section id="first">
+        <div class="container">
+            <div class="row d-flex justify-content-center mt-5">
+                <div class="col-md-12">
+                    <h2 class="text-center text-overview">Overview</h2>
+                </div>
+                <div class="col-md-12">
+                    <p class="text-center">With more than 85 components, over 45 available plugins, several directives, and 1200+ icons, 
+                        <span class="purple-text">BootstrapVue</span> 
+                        provides one of the most comprehensive implementations of the 
+                        <span class="purple-text">Bootstrap v4</span>
+                        component and grid system available for , 
+                        <span class="purple-text">Vue.js v2.6</span>
+                        complete with extensive and automated 
+                        <a href=""><abbr title="Web Accessibility Initiative – Accessible Rich Internet Applications" data-v-792738cd="">WAI-ARIA</abbr></a>
+                        accessibility markup.
+                    </p>
+                </div>
+            </div>
+            <div class="row d-flex justify-content-center text-center my-2">
+                <div class="col-md-4 py-3 offer">
+                    <button class="btn btn-outline btn-block violet">Get started</button>
+                </div>
+                <div class="col-md-4 py-3 offer">
+                    <button class="btn btn-outline btn-block violet">Playground</button>
+                </div>
+                <div class="col-md-4 py-3 offer">
+                    <button class="btn btn-outline btn-block">GitHub</button>
+                </div>
+            </div>
+            <div class="row d-flex justify-content-center mt-5">
+                <div class="col-md-6 d-sm-none d-md-block offset-md-3 col-sm-12 offset-sm-6 d-none d-sm-block">
+                    <div class="carbon d-flex">
+                        <img src="https://cdn4.buysellads.net/uu/1/81016/1609783196-authentic-260x200-variation-3.jpg" alt="ads via Carbon" border="0" height="100" width="130" style="max-width: 130px;">
+                        <a class="ml-3 carbon-text" href="https://srv.carbonads.net/ads/click/x/GTND42JJFTSDL27WFTSLYKQNC6ADE23LCK7ITZ3JCYYIE53EFT7D453KCK7IEKJNCKBD4KQIF6SDV5QNF6ADEK3KC6SD453UCKSDVK3EHJNCLSIZ?segment=placement:bootstrap-vuejsorg;" target="_blank" rel="noopener sponsored">Your new development career awaits. Check out the latest listings.</a>
+                    </div>
+                </div>
+            </div>
+            </div>
+    </section>
+
+    <section id="second">
+        <div class="container">
+            <h2 class="text-center text-muted">Reasons to choose BootstrapVue</h2>
+            <div class="row d-flex justify-content-center mt-1">
+                <div class="col-md-4">
+                    <div class="card card rounded-0 border-0">
+                        <div class="card-body">
+                            <h3 class="text-center">
+                                <img src="../assets/images/second-section-images/responsive.svg" alt="">
+                               <br> <br>
+                               <span>Responsive</span>
+                            </h3>
+                            <p class="text-center mt-2">Mobile first <a href="">responsive</a> layout</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card card rounded-0 border-0">
+                        <div class="card-body">
+                            <h3 class="text-center">
+                                <img src="../assets/images/second-section-images/modular.svg" alt="">
+                               <br> <br>
+                               <span>Modular</span>
+                            </h3>
+                            <p class="text-center mt-2"><a href="">Import</a> only the features that you need</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card card rounded-0 border-0">
+                        <div class="card-body">
+                            <h3 class="text-center">
+                                <img src="../assets/images/second-section-images/accessible.svg" alt="">
+                               <br> <br>
+                               <span>Accessible</span>
+                            </h3>
+                            <p class="text-center mt-2">Automated <a href="">WAI-ARIA accessibility </a> markup</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card card rounded-0 border-0">
+                        <div class="card-body">
+                            <h3 class="text-center">
+                                <img src="../assets/images/second-section-images/modern.svg" alt="">
+                               <br> <br>
+                               <span>Modern</span>
+                            </h3>
+                            <p class="text-center mt-2">Built with <a href=""> Vue.js v2.6</a> and <a href="">  Bootstrap SCSS v4.5</a></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card card rounded-0 border-0">
+                        <div class="card-body">
+                            <h3 class="text-center">
+                                <img src="../assets/images/second-section-images/configurable.svg" alt="">
+                               <br> <br>
+                               <span>Configurable</span>
+                            </h3>
+                            <p class="text-center mt-2">Create <a href="">themes</a> with SCSS variables and <a href="">global options</a></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card card rounded-0 border-0">
+                        <div class="card-body">
+                            <h3 class="text-center">
+                                <img src="../assets/images/second-section-images/free.svg" alt="">
+                               <br> <br>
+                               <span>Free</span>
+                            </h3>
+                            <p class="text-center mt-2">Open sourced on <a href="">GitHub, MIT</a> License</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="third">
+        <div class="container">
+            <h2 class="text-center">Integrate with Nuxt.js</h2>
+                <div class="mb-2 d-flex flex-row justify-content-center align-items-center">
+                        <img src="../assets/images/nuxt.svg" alt="">
+                        <b>+</b>
+                        <img src="../assets/images/vue-bootstrap.svg" alt="">
+                </div>
+                <div class="row d-flex justify-content-center">
+                    <div class="col-md-12 text-center">
+                        <p class="text-lg">Easily integrate BootstrapVue into your Nuxt.js projects using our included Nuxt.js module. You can optionally specify only the components, directives and/or plugins you require.</p>
+                        <a href="" class="btn mb-3 btn-outline-secondary btn-sm"> BootstrapVue Nuxt.js module </a>
+                        <p>BootstrapVue's documentation is created using <span class="purple-text">BootstrapVue</span> and generated with <a href="">Nuxt.js</a></p>
+                    </div>
+                </div>
+        </div>
+    </section>
+
+    <section id="forth">
+        <div class="container">
+            <div class="row d-flex justify-content-center">
+                <div class="col-md-12 text-center">
+                   <h2 class="mb-3"><img src="../assets/images/circle-purple.svg" alt="">
+                        <br> Support BootstrapVue on Open Collective
+                   </h2>
+                    <a href="https://opencollective.com/bootstrap-vue#sponsor" rel="noopener" target="_blank" class="btn mb-2 mr-1 btn-outline-secondary btn-sm" data-v-792738cd="">
+                        Become a sponsor
+                      </a>
+                      <a href="https://opencollective.com/bootstrap-vue#sponsor" rel="noopener" target="_blank" class="btn mb-2 mr-1 btn-outline-secondary btn-sm" data-v-792738cd="">
+                        Become a sponsor
+                      </a>
+                      <a href="https://opencollective.com/bootstrap-vue#donate" rel="noopener" target="_blank" class="btn mb-2 btn-outline-secondary btn-sm" data-v-792738cd="">
+                        Donate
+                      </a>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="fifth">
+        <footer>
+            <div class="container">
+                <div class="row d-flex justify-content-center">
+                    <div class="col-md-3 mt-5">
+                        <h5>Docomentation</h5>
+                        <ul class="list-unstyled">
+                            <li>
+                                <a href="">Home</a>
+                            </li>
+                            <li>
+                                <a href="">Getting started</a>
+                            </li>
+                            <li>
+                                <a href="">Components</a>
+                            </li>
+                            <li>
+                                <a href="">Directives</a>
+                            </li>
+                            <li>
+                                <a href="">Icons</a>
+                            </li>
+                            <li>
+                                <a href="">Referance</a>
+                            </li>
+                            <li>
+                                <a href="">Playground</a>
+                            </li>
+                        </ul>
+                        <h5>Themes</h5>
+                        <ul class="list-unstyled">
+                            <li>
+                                <a href="">Themes and dashboards</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-md-3 mt-5">
+                        <h5>Community</h5>
+                        <ul class="list-unstyled">
+                            <li>
+                                <a href="">GitHub</a>
+                            </li>
+                            <li>
+                                <a href="">Discord chat</a>
+                            </li>
+                            <li>
+                                <a href="">Twitter</a>
+                            </li>
+                            <li>
+                                <a href="">Open Collective</a>
+                            </li>
+                        </ul>
+                        <h5>Release notes</h5>
+                        <ul class="list-unstyled">
+                            <li>
+                                <a href="">Changelog</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-md-4 text-sm-right">
+                        <img src="../assets/images/big-logo.svg" alt="">
+                    </div>
+                </div>
+                <hr>
+                <p class="text-center">Designed and built with all the love in the world. Maintained by the <a href="">core team</a>  with the help of <a href="">our contributors</a>.</p>
+                <p class="text-center">Currently v2.21.2. Code licensed<a href="">MIT</a> . Docs generated with <a href="">Nuxt.js</a> and proudly hosted on <a href="">Vercel</a>.</p>
+                <p class="text-center">
+                    <a href="">
+                        <img src="../assets/images/powered-bt-vercel.svg" width="159" height="33">
+                    </a>
+                </p>
+            </div>
+        </footer>
+    </section>
+  </div>
+</template>
